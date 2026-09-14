43ad14df1e425279a1978cfdb22d4918fb23257b
Junaid Khan <khan13461727@gmail.com>
2026-09-14 07:25:56 +0500

    update

--- class.py
+++ class.py
@@ -1,10 +1,3 @@
-class Car:
-   carmodel="2027"
-def __init__(self,color,model):
-   self.color=color
-   self.model=model
-
-
-car1=Car("black","Mercedes")
-print(car1.color)
+print("Hello world!")
+print("junaid hanif")
    
